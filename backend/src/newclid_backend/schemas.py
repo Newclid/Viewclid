acb5e48931186d9bc231e74e95ab930817a03753
Simeon Vutov <s.vutov@student.tudelft.nl>
2026-05-13 13:26:11 +0200

    feat: added a schema for the request to create a job

---
+++ backend/src/newclid_backend/schemas.py
@@ -0,0 +1,20 @@
+from typing import Any, Literal
+
+from pydantic import BaseModel, Field
+
+class CreateJobRequest(BaseModel):
+    input_type: Literal["jgex"] = Field(
+        default="jgex",
+        description="Input format of the submitted problem"
+    )
+    problem_input: str = Field(
+        description="Problem definition in JGEX format"
+    )
+    goals: str = Field(
+        description="Goal that Newclid should try to prove."
+    )
+    timeout_seconds: int = Field(
+        default=120,
+        description="Timeout time after which the newclid process will be stopped"
+    )
+
